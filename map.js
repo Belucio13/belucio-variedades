a1096836641e0f9c857cd5bd5a5e2338205176fd
Eduardo Schwanke <carvalhoe089@gmail.com>
2021-10-30 09:16:40 -0300

    adicionando ao github

---
+++ map.js
@@ -0,0 +1,32 @@
+var L;
+
+window.onload = function() {
+L.mapquest.key = '18gIpGQcFa9B9JVf0TAv664fI4StOKEI';
+var map = L.mapquest.map('map', {
+    center: [-1.300307, -48.482530],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 15
+});
+
+
+L.mapquest.textMarker([-1.300507, -48.482830], {
+text: 'Belucio Variedades',
+position: 'right',
+type: 'marker',
+icon: {
+    primaryColor: '#095f03',
+    secondaryColor: '#09b403',
+    shadow: true,
+    size: 'md',
+    symbol: 'B'
+}
+})
+.bindPopup(`<img src="frente-loja.jpg"> 
+            <h3>Loja Belucio Variedades</h3>
+            <p>Loja de variedades</p> 
+            <p>Aberto das 8:00 às 20:00</p>
+            <p>R. Vip, 94</p> 
+            <p>whatsapp: xxxxx-xxxx</p>
+        `)
+.addTo(map)
+}
