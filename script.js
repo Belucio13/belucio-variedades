36e9b654a8f5152657e9548185c5a26dc9f0acb2
Eduardo Schwanke <carvalhoe089@gmail.com>
2021-11-01 10:08:26 -0300

    container produtos desktop finalizado

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const menu = document.querySelectorAll('.item p');
+const container_produtos = document.querySelectorAll('.todos-produtos');
+
  
+const limparProdutos = () => {
+    container_produtos.forEach(produtos => {
+        produtos.style.display = 'none';
+    })
+}
+
+const resetarMenuBorda = () => {
+    menu.forEach(menu => {
+        menu.style.borderBottom = 'none';
+    })
+}
+
+const showProdutos = (event) => {
+    limparProdutos();
+    resetarMenuBorda();
+    container_produtos.forEach(produto => {
+        if(produto.id == event.target.innerHTML){
+            produto.style.display = 'flex';
+            menu.forEach(menu => {
+                if(produto.id == menu.innerHTML){
+                    menu.style.borderBottom = '1px solid green'
+                }
+            })
+        }
+    })
+}
+
+
+ 
+menu.forEach(produto => {
+     produto.addEventListener('click', showProdutos);
+})
