57b04642bb209fa5ed713e025143e206c288cc93
Eduardo Schwanke <carvalhoe089@gmail.com>
2021-12-06 15:17:59 -0300

    icons

--- scripts/search.js
+++ scripts/search.js
@@ -5,29 +5,27 @@ const hiddenSearch = () => {
     document.querySelector('.logo-mobile').style.display = 'block'
     document.querySelector('.hamburguer').style.display = 'block'
     document.querySelector('#pesquisar-mobile').style.backgroundColor = 'rgba(0,0,0,0)'
-    document.querySelector('.search-mobile i').style.color = 'white'
-    document.querySelector('.search-mobile .xis').style.right = '93%'
-    document.querySelector('.search-mobile .xis').style.display = 'none'
-    document.querySelector('.search-mobile i').style.left = '23%'
-    document.querySelector('.search-mobile').style.width = '8%'
-    document.querySelector('.search-mobile input').style.width = '10%'
+    document.querySelector('#pesquisar-mobile').style.width = '34px'
+    document.querySelector('.search-mobile').style.width = '34px'
+    document.querySelector('.ball-lupa').style.border = '2px solid white'
+    document.querySelector('.line-lupa').style.backgroundColor = 'white'
+    document.querySelector('.xis').style.display = 'none'
+    document.querySelector('.xis').style.right = '30px'
     document.querySelector('#pesquisar-mobile').disabled = true
 }
 
-document.querySelector('.fa-search')
+document.querySelector('.lupa')
     .addEventListener('click', () => {
-    document.querySelector('.logo-mobile').style.display = 'none'
-    document.querySelector('.hamburguer').style.display = 'none'
-    document.querySelector('#pesquisar-mobile').style.backgroundColor = 'white'
-    document.querySelector('.search-mobile i').style.color = 'black'
-    document.querySelector('.search-mobile .xis i').style.color = 'black'
-    document.querySelector('.search-mobile .xis').style.right = '7%'
-    document.querySelector('.search-mobile .xis').style.display = 'block'
-    document.querySelector('.search-mobile i').style.left = '2%'
-    document.querySelector('.search-mobile').style.width = '100%'
-    document.querySelector('.search-mobile input').style.width = '100%'
-    document.querySelector('#pesquisar-mobile').disabled = false
-    document.querySelector('#pesquisar-mobile').focus()
+        document.querySelector('.logo-mobile').style.display = 'none'
+        document.querySelector('.hamburguer').style.display = 'none'
+        document.querySelector('#pesquisar-mobile').style.backgroundColor = 'white'
+        document.querySelector('#pesquisar-mobile').style.width = '100%'
+        document.querySelector('.search-mobile').style.width = '100%'
+        document.querySelector('.ball-lupa').style.border = '2px solid black'
+        document.querySelector('.line-lupa').style.backgroundColor = 'black'
+        document.querySelector('.xis').style.display = 'block'
+        document.querySelector('.xis').style.right = '30px'
+        document.querySelector('#pesquisar-mobile').disabled = false
 })
 
-document.querySelector('.xis i').addEventListener('click', hiddenSearch)
+document.querySelector('.xis').addEventListener('click', hiddenSearch)
